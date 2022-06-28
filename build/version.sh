38b2d80f7829bb0e3181fa252f1d9025d46e6604
Shigma <1700011071@pku.edu.cn>
2022-06-29 01:54:21 +0800

    fix: use binary data

--- build/version.sh
+++ build/version.sh
@@ -1,4 +1,7 @@
 RAW=v$(cat package.json | grep '"version":' | cut -d '"' -f 4)
 REG=^$(echo $RAW | sed 's/\./\\./g')$
-NEW=$([ -z "$(git tag -l | grep -E $REG)" ] && echo "$RAW")
-echo "::set-output name=tag::$NEW"
+
+if [ -z "$(git tag -l | grep -E $REG)" ]; then
+  echo ::set-output name=tag::$RAW
+  echo change detected: $RAW
+fi
--- src/release.ts
+++ src/release.ts
@@ -1,8 +1,8 @@
 import { context, getOctokit } from '@actions/github'
 import { getInput } from '@actions/core'
-import { createReadStream } from 'fs'
 import { resolve } from 'path'
 import manifest from './shared'
+import { readFile } from 'fs/promises'
 
 const token = getInput('github_token', { required: true })
 
@@ -11,22 +11,24 @@ const octokit = getOctokit(token)
 
 export default async function release() {
   const tag = `v${manifest.version}`
-  await octokit.rest.git.createTag({
+  const { data } = await octokit.rest.git.createTag({
     ...repo,
     tag,
     message: `Release ${manifest.version}`,
     object: sha,
     type: 'commit',
   })
+  console.log(data)
   const { data: release } = await octokit.rest.repos.createRelease({
     ...repo,
     tag_name: tag,
   })
+  console.log(release)
   await octokit.rest.repos.uploadReleaseAsset({
     ...repo,
     release_id: release.id,
     name: tag,
-    data: createReadStream(resolve(process.env.RUNNER_TEMP, 'bundle.zip')) as any,
+    data: await readFile(resolve(process.env.RUNNER_TEMP, 'bundle.zip')) as any,
   })
 }
 
