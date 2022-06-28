426711f15b27c867a933438c666b0a68e3f3e74a
Shigma <shigma10826@gmail.com>
2022-06-29 00:51:17 +0800

    chore: add missing dependencies

--- src/shared.ts
+++ src/shared.ts
@@ -1,7 +1,9 @@
+import { readFileSync } from 'fs'
+
 interface Manifest {
   name: string
   version: string
   files: string[]
 }
 
-export default require('./package.json') as Manifest
+export default JSON.parse(readFileSync('package.json', 'utf-8')) as Manifest
